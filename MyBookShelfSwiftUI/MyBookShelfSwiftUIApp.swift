eb75bbe5490a9004a71e7665d4005ff5f70a9276
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-07 14:19:20 -0700

    Initial Commit

---
+++ MyBookShelfSwiftUI/MyBookShelfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookShelfSwiftUIApp.swift
+//  MyBookShelfSwiftUI
+//
+//  Created by antikiller on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyBookShelfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
